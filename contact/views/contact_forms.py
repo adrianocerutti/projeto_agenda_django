2f1f34bb631ecda161efdf56d1f10f1328b8ebaa
Adriano B Cerutti <adrianocerutti@gmail.com>
2024-11-18 09:46:18 -0300

    Movendo o ContactForm para forms.py

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,15 +1,5 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator
-from django.core.exceptions import ValidationError
-from django.db.models import Q
-from contact.models import Contact
-from django import forms
-
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ('first_name', 'last_name', 'phone',)
+from django.shortcuts import render
+from contact.forms import ContactForm
 
 
 def create(request):
